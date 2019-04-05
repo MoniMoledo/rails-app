851b979156e7f5a0c4ce328595fa7feb589e9c5c
MoniMoledo <moniquemoledo@hotmail.com>
2019-04-05 15:17:21 -0400

    Add show article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,14 +2,21 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+
+
     def create
         @article = Article.new(article_params) 
         if @article.save
             flash[:notice] = "Article successfully created"
-            redirect_to articles_path(@article)
+            redirect_to article_path(@article)
         else
             render :new
         end
+    end
+
+
+    def show
+        @article = Article.find(params[:id])
     end
 
     private
